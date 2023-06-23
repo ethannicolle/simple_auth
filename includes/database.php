cec85dba796f7715b790faa61746c65d0af9b3cd
Ethan Nicolle <ethan.nicolle.pro@gmail.com>
2023-06-23 03:18:16 +0200

    No changes

--- includes/database.php
+++ includes/database.php
@@ -7,9 +7,9 @@ const DBNAME = 'test_db';
 
 $dsn = 'mysql:host=' . DBHOST . ';dbname=' . DBNAME;
 
-try{
+try {
     $db = new PDO($dsn, DBUSER, DBPASS);
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-}catch(PDOException $e) {
+} catch (PDOException $e) {
     die($e->getMessage());
 }
